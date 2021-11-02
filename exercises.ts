a72848888d37ead76e8199e0334e4010ee4c4dfb
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-02 19:27:00 +0200

    exe 2

---
+++ exercises.ts
@@ -0,0 +1,75 @@
+/*
+Exercise:
+   Given the data, define the interface "User" and use it accordingly.
+*/
+/* interface User {
+    name: string;
+    age: number;
+    occupation: string;
+};
+
+const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+const logPerson = (user: User) => console.log(` - ${user.name}, ${user.age}`);
+console.log('Users:');
+users.forEach(logPerson); */
+
+/*
+Exercise:
+    Type "Person" is missing, please define it and use
+    it in persons array and logPerson function in order to fix
+    all the TS errors.
+*/
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(user: Person) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+persons.forEach(logPerson);

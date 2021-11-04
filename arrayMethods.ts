a7574d80b1330370c4e06021e7113057ced00055
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-04 15:29:49 +0200

    forEach()

--- arrayMethods.ts
+++ arrayMethods.ts
@@ -3,7 +3,7 @@ import { push } from './push';
 //** Filter */
 type FilterCallback<T> = (value: T, index: number, array: T[]) => boolean;
 
-export function filter<T>(array: T[], callback: FilterCallback<T>) {
+ function filter<T>(array: T[], callback: FilterCallback<T>) {
   const result: any[] = [];
 
   const { length } = array;
@@ -17,4 +17,17 @@ export function filter<T>(array: T[], callback: FilterCallback<T>) {
   }
 
   return result;
+}
+
+//** ForEach */
+type ForEachCallback<T> = (value: T, index: number, array: T[]) => void;
+
+ function forEach<T>(array: T[], callback: ForEachCallback<T>) {
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    callback(value, index, array);
+  }
 }

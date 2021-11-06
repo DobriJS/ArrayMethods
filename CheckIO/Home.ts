609096b41d71cff46a75d4f978af11d0125e4c2a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-06 15:46:01 +0200

    Nearest Value

--- CheckIO/Home.ts
+++ CheckIO/Home.ts
@@ -22,3 +22,18 @@ assert.equal(correctSentence('Greetings, friends.'), 'Greetings, friends.'); */
 assert.equal(isAcceptablePassword('short'), false);
 assert.equal(isAcceptablePassword('muchlonger'), true);
 assert.equal(isAcceptablePassword('ashort'), false); */
+
+//-----
+/* function nearestValue(values: number[], search: number): number {
+    let arr = values.sort((x, y) => x - y).map(x => Math.abs(x - search));
+
+  return values[arr.indexOf(Math.min(...arr))];
+}
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 9), 10);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 8), 7);
+assert.equal(nearestValue([4, 8, 10, 11, 12, 17], 9), 8);
+assert.equal(nearestValue([4, 9, 10, 11, 12, 17], 9), 9);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 0), 4);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 100), 17);
+assert.equal(nearestValue([5, 10, 8, 12, 89, 100], 7), 8);
+assert.equal(nearestValue([-1, 2, 3], 0), -1); */

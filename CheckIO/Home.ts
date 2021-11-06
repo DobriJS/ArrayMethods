56b025a8ec9ba9a9471846fec0da21c4142441e5
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-06 14:46:44 +0200

    correctSentence

--- CheckIO/Home.ts
+++ CheckIO/Home.ts
@@ -1,8 +1,15 @@
-function replaceFirst(values: number[]): number[] {
+/* function replaceFirst(values: number[]): number[] {
     const [first, ...rest] = values;
     return first ? [...rest, first] : [];
  }
-
-/* assert.deepEqual(replaceFirst([1, 2, 3, 4]), [2, 3, 4, 1]);
+assert.deepEqual(replaceFirst([1, 2, 3, 4]), [2, 3, 4, 1]);
 assert.deepEqual(replaceFirst([1]), [1]);
-assert.deepEqual(replaceFirst([]), []); */
+assert.deepEqual(replaceFirst([]), []);  */
+
+//------
+function correctSentence(text: string): string {
+    return text[0].toUpperCase() + text.slice(1) + (text.slice(-1) == '.' ? '' : '.');
+ }
+assert.equal(correctSentence('greetings, friends'), 'Greetings, friends.');
+assert.equal(correctSentence('Greetings, friends'), 'Greetings, friends.');
+assert.equal(correctSentence('Greetings, friends.'), 'Greetings, friends.');

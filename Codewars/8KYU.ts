9e3a82a1a5f2643346d6553c560c10e658ecf166
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-08 12:47:25 +0200

    Reversed Strings

---
+++ Codewars/8KYU.ts
@@ -0,0 +1,8 @@
+// Reversed Strings
+ function solution(str: string): string {
+    let newString = "";
+      for (let i = str.length - 1; i >= 0; i--) {
+          newString += str[i];
+      }
+      return newString;
+  }

9663ba943c6116a39f378ab351a6199e2975cdde
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-11 12:10:45 +0200

    Thinking & Testing: A and B?

--- Codewars/7KYU.ts
+++ Codewars/7KYU.ts
@@ -2,3 +2,8 @@
 function descendingOrder(n: number): number {
     return parseInt(n.toString().split('').sort().reverse().join(''));
   }
+
+// Thinking & Testing: A and B?
+function testit(a: number, b: number): number {
+  return a | b ;
+}

0218bea81219acc649015c02654294d9a4acea08
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-26 17:50:19 +0200

    Coding Meetup #4

--- Codewars/HOF.ts
+++ Codewars/HOF.ts
@@ -22,7 +22,7 @@
 
 // Coding Meetup #2 - Higher-Order Functions Series - Greet developers
 
-const list1 = [
+/* const list1 = [
     { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
     { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
     { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
@@ -43,4 +43,26 @@ const list1 = [
         dev.greeting = `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`;
     })
     return list;
+  } */
+
+// Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+const list1 = [
+    { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+    { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+  ];
+
+  interface DeveloperList {
+    firstName: string;
+    lastName: string;
+    country: string;
+    continent: string;
+    age: number;
+    language: string;
   }
+
+function getFirstPython(list: DeveloperList[]): string {
+    const dev = list.find(x => x.language === "Python");
+    return dev ? `${dev.firstName}, ${dev.country}` : "There will be no Python developers";
+   }
+   console.log(getFirstPython(list1));

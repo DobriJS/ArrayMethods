f7db1b90942efc09fd5d467259fd4ef7e24b7a83
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-11-28 15:20:22 +0200

    oding Meetup #6

--- Codewars/HOF.ts
+++ Codewars/HOF.ts
@@ -46,7 +46,7 @@
   } */
 
 // Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
-const list1 = [
+/* const list1 = [
     { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
     { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
     { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
@@ -65,4 +65,26 @@ function getFirstPython(list: DeveloperList[]): string {
     const dev = list.find(x => x.language === "Python");
     return dev ? `${dev.firstName}, ${dev.country}` : "There will be no Python developers";
    }
-   console.log(getFirstPython(list1));
+   console.log(getFirstPython(list1)); */
+
+// Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?
+
+const list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+interface DeveloperList {
+  firstName: string;
+  lastName: string;
+  country: string;
+  continent: string;
+  age: number;
+  language: string;
+}
+
+function isSameLanguage(list: DeveloperList[]): boolean {
+  return list.every(e => e.language === list[0].language);
+}
+console.log(isSameLanguage(list1));

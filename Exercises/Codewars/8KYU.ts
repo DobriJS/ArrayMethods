383ea6c1f78d8da99d1f0e194fbe63bbf695e7b2
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-12-06 17:21:06 +0200

    Plural

--- Exercises/Codewars/8KYU.ts
+++ Exercises/Codewars/8KYU.ts
@@ -79,3 +79,8 @@ const centuryFromYear = (year: number): number => {
 function correct(s: string): string {
   return s.replace(/0/g, 'O').replace(/1/g, 'I').replace(/5/g, 'S');
 }
+
+// Plural
+function plural(n: number): boolean {
+  return n !== 1;
+}

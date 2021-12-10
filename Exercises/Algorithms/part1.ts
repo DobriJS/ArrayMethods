06df1bd4d25fb1ef91c16344c18ac2930554cd95
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-12-10 16:21:49 +0200

    Array Conversion

--- Exercises/Algorithms/part1.ts
+++ Exercises/Algorithms/part1.ts
@@ -102,3 +102,32 @@ console.log(almostIncreasingSequence([1,3,2])); */
     return [evenSum, oddSum];
 }
 console.log(alternatingSums([50, 60, 60, 45, 70])); */
+
+// Array Conversion
+// inputArray = [1,2,3,4,5,6,7,8] output should be 186
+// [1,2,3,4,5,6,7,8] -> [3,7,11,15] -> [21,156] -> [186]
+function arrayConversion(inputArray: number[]): number {
+  let isOdd = true;
+
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
+  return inputArray[0];
+}
+
+function sumProduct(nums: number[], isOdd: boolean): number[] {
+  const sumProducts: number[] = [];
+
+  if (isOdd) {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] + nums[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] * nums[i + 1]);
+    }
+  }
+  return sumProducts;
+}
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));

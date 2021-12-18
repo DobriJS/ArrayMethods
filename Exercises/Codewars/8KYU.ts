c5bc96c13ebfb152791532c8754772a43b163b45
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-12-18 16:50:43 +0200

    Calculate BMI

--- Exercises/Codewars/8KYU.ts
+++ Exercises/Codewars/8KYU.ts
@@ -129,3 +129,15 @@ function finalGrade(exam: number, projects: number): number {
     return 0;
   }
 }
+
+// Calculate BMI
+function bmi(weight: number, height: number): string {
+  // TODO: implement this method
+  const bmi = weight / Math.pow(height, 2);
+
+  if (bmi <= 18.5) return 'Underweight';
+  else if (bmi <= 25.0) return 'Normal';
+  else if (bmi <= 30.0) return 'Overweight';
+  else if (bmi > 30) return 'Obese';
+  else return '';
+}

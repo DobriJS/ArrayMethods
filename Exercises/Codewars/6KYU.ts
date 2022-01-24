cc1a1a0e3f3947e0a7f76159ea07ecc8e11a0a00
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-01-24 10:22:32 +0200

    Build Tower

--- Exercises/Codewars/6KYU.ts
+++ Exercises/Codewars/6KYU.ts
@@ -8,3 +8,12 @@ function wave(str: string): Array<string> {
     return wave;
   }, []);
 }
+
+// Build Tower
+const towerBuilder = (n: number): string[] => {
+  const result: string[] = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(' '.repeat(n - i) + '*'.repeat(i * 2 - 1) + ' '.repeat(n - i));
+  }
+  return result;
+};

23cefbfe8eacfffb9954ca480653f17350c41a5a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-13 13:22:36 +0300

    Collision Detection

--- Exercises/Codewars/7KYU.ts
+++ Exercises/Codewars/7KYU.ts
@@ -335,3 +335,15 @@ class G964 {
     return r.length + '/' + s.length;
   }
 }
+
+// Collision Detection
+function collision(
+  x1: number,
+  y1: number,
+  r1: number,
+  x2: number,
+  y2: number,
+  r2: number
+): boolean {
+  return Math.hypot(x2 - x1, y2 - y1) <= r1 + r2;
+}

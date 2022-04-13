d5739b5844d37c24270f9d704cbf7edacbabcb9e
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-13 18:38:53 +0300

    Annalyn's Infiltration

--- Exercises/Exercism/part1_JS.js
+++ Exercises/Exercism/part1_JS.js
@@ -1,4 +1,4 @@
-// Lucian's Luscious Lasagna
+// Lucian's Luscious Lasagna --------- //
 
 const PREPARATION_MINUTES_PER_LAYER = 2;
 export const EXPECTED_MINUTES_IN_OVEN = 40;
@@ -33,4 +33,67 @@ export function preparationTimeInMinutes(numberOfLayers) {
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
   return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+}
+
+// Annalyn's Infiltration ----------- //
+
+/**
+ * The fast attack is available when the knight is sleeping
+ *
+ * @param {boolean} knightIsAwake
+ *
+ * @return {boolean} Whether or not you can execute a fast attack.
+ */
+export function canExecuteFastAttack(knightIsAwake) {
+  if (!knightIsAwake) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  return knightIsAwake || archerIsAwake || prisonerIsAwake;
+}
+
+/**
+ * You'll get caught by the archer if you signal while they're awake.
+ *
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can send a signal to the prisoner.
+ */
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  return !archerIsAwake && prisonerIsAwake;
+}
+
+/**
+ * The final stage in the plan: freeing Annalyn's best friend.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * @param {boolean} petDogIsPresent
+ *
+ * @returns {boolean} Whether or not you can free Annalyn's friend.
+ */
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+  return (
+    (prisonerIsAwake && !knightIsAwake && !archerIsAwake) ||
+    (!archerIsAwake && petDogIsPresent)
+  );
 }

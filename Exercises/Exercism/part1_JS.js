cfa4f7c2e14b626bdc036cf92921e8e6b323ea3c
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-14 21:33:25 +0300

    BankAccount

--- Exercises/Exercism/part1_JS.js
+++ Exercises/Exercism/part1_JS.js
@@ -97,3 +97,54 @@ export function canFreePrisoner(
     (!archerIsAwake && petDogIsPresent)
   );
 }
+
+// Bank Account -------------- //
+
+export class BankAccount {
+  constructor() {
+    this._balance = 0;
+    this.isOpen = false;
+  }
+
+  open() {
+    if (this.isOpen) {
+      throw new ValueError();
+    }
+    this.isOpen = true;
+  }
+
+  close() {
+    if (!this.isOpen) {
+      throw new ValueError();
+    }
+    this.isOpen = false;
+    this._balance = 0;
+  }
+
+  deposit(amount) {
+    if (!this.isOpen || amount < 0) {
+      throw new ValueError();
+    }
+    this._balance += amount;
+  }
+
+  withdraw(amount) {
+    if (!this.isOpen || amount > this._balance || amount < 0) {
+      throw new ValueError();
+    }
+    this._balance -= amount;
+  }
+
+  get balance() {
+    if (!this.isOpen) {
+      throw new ValueError();
+    }
+    return this._balance;
+  }
+}
+
+export class ValueError extends Error {
+  constructor() {
+    super('Bank account error');
+  }
+}

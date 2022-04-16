02c96bf321d3254673189731cb74f1bf9d359dae
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-16 13:15:08 +0300

    Lucky Numbers

--- Exercises/Exercism/part1_JS.js
+++ Exercises/Exercism/part1_JS.js
@@ -268,3 +268,42 @@ export function fixBirdCountLog(birdsPerDay) {
   }
   return birdsPerDay;
 }
+
+// Lucky Numbers -------------- //
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  const slot1 = array1.join('');
+  const slot2 = array2.join('');
+  return Number(slot1) + Number(slot2);
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean}  whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  const reversed = String(value).split('').reverse().join('');
+  return reversed == value;
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  if (!input) return 'Required field';
+  if (input == 0 || !Number(input)) return 'Must be a number besides 0';
+  return '';
+}

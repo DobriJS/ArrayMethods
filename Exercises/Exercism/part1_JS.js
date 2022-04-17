1021029b3671a39192e08d03c9ece3eb58db6317
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-17 17:16:50 +0300

    Elyses Analytic Enchantments

--- Exercises/Exercism/part1_JS.js
+++ Exercises/Exercism/part1_JS.js
@@ -381,3 +381,29 @@ function removeItemAtBottom(cards) {
 function checkSizeOfStack(cards, stackSize) {
   return cards.length === stackSize;
 }
+
+// Elyses Analytic Enchantments --------------- //
+
+function getCardPosition(stack, card) {
+  return stack.findIndex((idx) => idx === card);
+}
+
+function doesStackIncludeCard(stack, card) {
+  return stack.includes(card);
+}
+
+function isEachCardEven(stack) {
+  return stack.every((card) => card % 2 === 0);
+}
+
+function doesStackIncludeOddCard(stack) {
+  return stack.some((card) => card % 2 !== 0);
+}
+
+function getFirstOddCard(stack) {
+  return stack.find((card) => card % 2 !== 0);
+}
+
+function getFirstEvenCardPosition(stack) {
+  return stack.findIndex((card) => card % 2 === 0);
+}

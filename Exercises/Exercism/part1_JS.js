a42a16afd16f9940860e6d048bfd14f2839f2f6c
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-18 17:37:19 +0300

    Darts

--- Exercises/Exercism/part1_JS.js
+++ Exercises/Exercism/part1_JS.js
@@ -436,3 +436,13 @@ function insertFaceCards(deck) {
   const [first, ...rest] = deck;
   return [first, ...FACE_CARDS, ...rest];
 }
+
+// Darts ----------------- //
+
+const score = (x, y) => {
+  const radius = Math.sqrt(x ** 2 + y ** 2);
+  if (radius > 10) return 0;
+  if (radius <= 1) return 10;
+  if (radius <= 5) return 5;
+  return 1;
+};

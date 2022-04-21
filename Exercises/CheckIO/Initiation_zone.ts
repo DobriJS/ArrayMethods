d9e9db4d9149946640e47978dde714db8387126c
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-04-21 19:00:30 +0300

    Easy Unpack

--- Exercises/CheckIO/Initiation_zone.ts
+++ Exercises/CheckIO/Initiation_zone.ts
@@ -2,3 +2,9 @@
 function firstWord(text: string): string {
   return text.split(' ')[0];
 }
+
+// Easy Unpack
+function easyUnpack(values: any[]): any[] {
+  // your code here
+  return [values[0], values[2], values[values.length - 2]];
+}
